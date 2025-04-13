11f900bf30fa7a5efc83211e36c96d7d1c3aca17
Romário Cordeiro Cabó <romariocabo2012@gmail.com>
2025-04-13 16:22:18 -0300

    first commit

---
+++ src/main/java/com/br/obitus_manager/ObitusManagerApplication.java
@@ -0,0 +1,13 @@
+package com.br.obitus_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObitusManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObitusManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/obitus_manager/ObitusManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.obitus_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObitusManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
